87b2f14f3cbca7e7d7c9acd6f98428c10bcb6fa4
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2021-08-28 11:28:30 +0200

    Add first files

---
+++ test-webscrap.py
@@ -0,0 +1,3 @@
+import requests
+r = requests.get('https://kamil.kwapisz.pl')
+print(r.status_code)
---
+++ test2.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('test/dataframes/Pokemon.csv')
+print(df.head(5))
