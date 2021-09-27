b6fbbee558b4de9e2f318752bdd6e0cb5b63c140
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2021-09-27 15:36:51 +0200

    Sorting methods

--- test-pandas.py
+++ test-pandas.py
@@ -1,3 +1,14 @@
 import pandas as pd
 df = pd.read_csv('dataframes/Pokemon.csv')
-print(df.head(15))
+
+# print(df.iloc[2,1])
+
+# for index, row in df.iterrows():
+#     print(index, row['Name'])
+
+# print(df.loc[df['Legendary'] == True])
+
+# print(df.describe())
+
+
+print(df.sort_values(['Type 1', 'HP'], ascending=[0,1]))
